1197c57f00f6708211daed2071c8f68d92894afb
march900901 <37949340+march900901@users.noreply.github.com>
2023-12-03 03:09:04 +0800

    測試刪除List中重複物件

--- Assets/Scrips/test.cs
+++ Assets/Scrips/test.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class test : MonoBehaviour
 {
+    public List<string> strings = new List<string>{"0","5","4","5","0","7","9"};
     // Start is called before the first frame update
     void Start()
     {
-        
+        //ConparList(strings);
+        ConparList2();
+        foreach (var item in strings)
+        {
+            print(item);
+        }
     }
 
     // Update is called once per frame
@@ -18,5 +25,27 @@ public class test : MonoBehaviour
 
     private void OnCollisionEnter(Collision other) {
         print(this.gameObject.name + "collistion");
+    }
+
+    public void ConparList(List<string> list){
+        HashSet<string> hs = new HashSet<string>(list);
+        foreach (string HashSet in hs)
+        {
+            print(HashSet);
+        }
+        
+    }
+
+    public void ConparList2(){
+        for (int i = 0; i < strings.Count; i++)
+        {
+            for (int j = strings.Count-1; j > i; j--)
+            {
+                if (strings[i] == strings[j])
+                {
+                    strings.RemoveAt(j);
+                }
+            }
+        }
     }
 }
