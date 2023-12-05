276f753a86da61865127608c865bae17b990c589
march900901 <37949340+march900901@users.noreply.github.com>
2023-12-05 15:39:58 +0800

    新增側邊碰撞器偵測

---
+++ Assets/Scrips/HitCollider.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun.Demo.PunBasics;
+using UnityEngine;
+
+public class HitCollider : MonoBehaviour
+{
+    public PlayerData _pd;
+    //public GameManager _gm;
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player")
+        {
+            _pd.OnHit(other.gameObject.GetComponent<PlayerData>());
+        }
+    }
+}
