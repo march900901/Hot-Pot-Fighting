aa1bc3701f333ab03e0135c7046ed05cb0c75f41
march900901 <37949340+march900901@users.noreply.github.com>
2023-12-14 23:29:07 +0800

    新增: 重新綁定按鍵功能

--- Assets/Scrips/SelectCharacterManager.cs
+++ Assets/Scrips/SelectCharacterManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 using System.Text;
 using Photon.Realtime;
 using UnityEngine.InputSystem;
+using Unity.VisualScripting;
 
 public class SelectCharacterManager : MonoBehaviourPunCallbacks
 {
@@ -16,14 +17,40 @@ public class SelectCharacterManager : MonoBehaviourPunCallbacks
     Text textPlayerList;
     [SerializeField]
     Button buttonStartGame;
+    [SerializeField]
+    GameObject rebindingPanel;
+    [SerializeField]
+    InputActionReference MoveActionRef;
+    [SerializeField]
+    InputActionReference DashActionRef;
+    [SerializeField]
+    InputActionReference LiftActionRef;
+    [SerializeField]
+    InputActionReference ThrowActionRef;
+    [SerializeField]
+    Text ForwordActionText;
+    [SerializeField]
+    Text BackwordActionText;
+    [SerializeField]
+    Text LeftActionText;
+    [SerializeField]
+    Text RightActionText;
+    [SerializeField]
+    Text DashActionText;
+    [SerializeField]
+    Text LiftActionText;
+    [SerializeField]
+    Text ThrowActionText;
     public int CharacterIndex = 0;
     public List<GameObject> CharacterList = new List<GameObject>();
     public Transform GeneratPoint;
     public string selectCharacterName;
     public Text text_Confirm;
+    
     public bool CanStart;
     public int CanStartPlayer;
     PhotonView _pv;
+    PlayerInput playerInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -42,6 +69,8 @@ public class SelectCharacterManager : MonoBehaviourPunCallbacks
         selectCharacterName = Character.name;//將所selectCharcterName設為當前所選角色名字
         text_Confirm.enabled = false;//將UI初始化
         _pv = this.transform.GetComponent<PhotonView>();
+        playerInput = this.transform.GetComponent<PlayerInput>();
+        SetBindingBT();
     }
 
     public override void OnMasterClientSwitched(Player newMasterClient){//當房主切換的時候
@@ -175,5 +204,229 @@ public class SelectCharacterManager : MonoBehaviourPunCallbacks
             print("Can't Start");
             buttonStartGame.interactable = false;
         }
+    }
+
+//--------綁定按鍵-------
+    public void SetBindingBT(){//初始化綁定按鈕顯示文字
+        var vec = MoveActionRef.action.ChangeCompositeBinding("2DVector");
+        var forword = vec.NextPartBinding("Up");
+        var backword = vec.NextPartBinding("Down");
+        var left = vec.NextPartBinding("Left");
+        var right = vec.NextPartBinding("Right");
+        var f = MoveActionRef.action.GetBindingDisplayString(forword.bindingIndex);
+        var b = MoveActionRef.action.GetBindingDisplayString(backword.bindingIndex);
+        var l = MoveActionRef.action.GetBindingDisplayString(left.bindingIndex);
+        var r = MoveActionRef.action.GetBindingDisplayString(right.bindingIndex);
+        var fs = InputControlPath.ToHumanReadableString(f,InputControlPath.HumanReadableStringOptions.OmitDevice);
+        var bs = InputControlPath.ToHumanReadableString(b,InputControlPath.HumanReadableStringOptions.OmitDevice);
+        var ls = InputControlPath.ToHumanReadableString(l,InputControlPath.HumanReadableStringOptions.OmitDevice);
+        var rs = InputControlPath.ToHumanReadableString(r,InputControlPath.HumanReadableStringOptions.OmitDevice);
+        ForwordActionText.text = fs;
+        BackwordActionText.text = bs;
+        LeftActionText.text = ls;
+        RightActionText.text = rs;
+
+        var dash = DashActionRef.action.bindings[0].effectivePath;
+        var lift = LiftActionRef.action.bindings[0].effectivePath;
+        var Throw = ThrowActionRef.action.bindings[0].effectivePath;
+        var dashBT = InputControlPath.ToHumanReadableString(dash,InputControlPath.HumanReadableStringOptions.OmitDevice);
+        var liftBT = InputControlPath.ToHumanReadableString(lift,InputControlPath.HumanReadableStringOptions.OmitDevice);
+        var throwBT = InputControlPath.ToHumanReadableString(Throw,InputControlPath.HumanReadableStringOptions.OmitDevice);
+        DashActionText.text = XboxController(dashBT);
+        LiftActionText.text = XboxController(liftBT);
+        ThrowActionText.text = XboxController(throwBT);
+    }
+
+    public void RebindingForword(){//重新綁定往前按鍵
+        print("Rebinding");
+        playerInput.SwitchCurrentActionMap("NotMe");
+        ForwordActionText.text = ("請輸入...");
+        var vec = MoveActionRef.action.ChangeCompositeBinding("2DVector");
+        var forword = vec.NextPartBinding("Up");
+            MoveActionRef.action.PerformInteractiveRebinding(forword.bindingIndex)
+            .OnMatchWaitForAnother(0.1f)
+            .WithCancelingThrough("<keyboard>/escape")
+            .OnComplete(operation=>{
+                var p = MoveActionRef.action.GetBindingDisplayString(forword.bindingIndex);
+                var s = InputControlPath.ToHumanReadableString(p,InputControlPath.HumanReadableStringOptions.OmitDevice);
+                string text = XboxController(s);
+                ForwordActionText.text = $"{text}";
+                print("done");
+                operation.Dispose();
+                playerInput.SwitchCurrentActionMap("Player1");
+            })
+            .OnCancel(operation=>{
+                var p = MoveActionRef.action.GetBindingDisplayString(forword.bindingIndex);
+                var s = InputControlPath.ToHumanReadableString(p,InputControlPath.HumanReadableStringOptions.OmitDevice);
+                string text = XboxController(s);
+                ForwordActionText.text = $"{text}";
+                print("done");
+                operation.Dispose();
+                playerInput.SwitchCurrentActionMap("Player1");
+            })
+            .Start();
+    }
+
+    public void RebindingBackword(){//重新綁定往後按鍵
+        print("Rebinding");
+        playerInput.SwitchCurrentActionMap("NotMe");
+        BackwordActionText.text = ("請輸入...");
+        var vec = MoveActionRef.action.ChangeCompositeBinding("2DVector");
+        var backword = vec.NextPartBinding("Down");
+        MoveActionRef.action.PerformInteractiveRebinding(backword.bindingIndex)
+        .OnMatchWaitForAnother(0.1f)
+        .WithCancelingThrough("<keyboard>/escape")
+        .OnComplete(operation=>{
+            var p = MoveActionRef.action.GetBindingDisplayString(backword.bindingIndex);
+            var s = InputControlPath.ToHumanReadableString(p,InputControlPath.HumanReadableStringOptions.OmitDevice);
+            string text = XboxController(s);
+            BackwordActionText.text = $"{text}";
+            print("done");
+            operation.Dispose();
+            playerInput.SwitchCurrentActionMap("Player1");
+        })
+        .OnCancel(operation=>{
+            var p = MoveActionRef.action.GetBindingDisplayString(backword.bindingIndex);
+            var s = InputControlPath.ToHumanReadableString(p,InputControlPath.HumanReadableStringOptions.OmitDevice);
+            string text = XboxController(s);
+            BackwordActionText.text = $"{text}";
+            print("done");
+            operation.Dispose();
+            playerInput.SwitchCurrentActionMap("Player1");
+        })
+        .Start();
+    }
+
+    public void RebindingLeft(){//重新綁定往左按鍵
+        print("Rebinding");
+        playerInput.SwitchCurrentActionMap("NotMe");
+        LeftActionText.text = ("請輸入...");
+        var vec = MoveActionRef.action.ChangeCompositeBinding("2DVector");
+        var Left = vec.NextPartBinding("Left");
+        MoveActionRef.action.PerformInteractiveRebinding(Left.bindingIndex)
+        .OnMatchWaitForAnother(0.1f)
+        .WithCancelingThrough("<keyboard>/escape")
+        .OnComplete(operation=>{
+            var p = MoveActionRef.action.GetBindingDisplayString(Left.bindingIndex);
+            var s = InputControlPath.ToHumanReadableString(p,InputControlPath.HumanReadableStringOptions.OmitDevice);
+            string text = XboxController(s);
+            LeftActionText.text = $"{text}";
+            print("done");
+            operation.Dispose();
+            playerInput.SwitchCurrentActionMap("Player1");
+        })
+        .OnCancel(operation=>{
+            var p = MoveActionRef.action.GetBindingDisplayString(Left.bindingIndex);
+            var s = InputControlPath.ToHumanReadableString(p,InputControlPath.HumanReadableStringOptions.OmitDevice);
+            string text = XboxController(s);
+            LeftActionText.text = $"{text}";
+            print("done");
+            operation.Dispose();
+            playerInput.SwitchCurrentActionMap("Player1");
+        })
+        .Start();
+    }
+
+    public void RebindingRght(){//重新綁定往右按鍵
+        print("Rebinding");
+        playerInput.SwitchCurrentActionMap("NotMe");
+        RightActionText.text = ("請輸入...");
+        var vec = MoveActionRef.action.ChangeCompositeBinding("2DVector");
+        var right = vec.NextPartBinding("Right");
+        MoveActionRef.action.PerformInteractiveRebinding(right.bindingIndex)
+        .OnMatchWaitForAnother(0.1f)
+        .WithCancelingThrough("<keyboard>/escape")
+        .OnComplete(operation=>{
+            var p = MoveActionRef.action.GetBindingDisplayString(right.bindingIndex);
+            var s = InputControlPath.ToHumanReadableString(p,InputControlPath.HumanReadableStringOptions.OmitDevice);
+            string text = XboxController(s);
+            RightActionText.text = $"{text}";
+            print("done");
+            operation.Dispose();
+            playerInput.SwitchCurrentActionMap("Player1");
+        })
+        .OnCancel(operation=>{
+             var p = MoveActionRef.action.GetBindingDisplayString(right.bindingIndex);
+            var s = InputControlPath.ToHumanReadableString(p,InputControlPath.HumanReadableStringOptions.OmitDevice);
+            string text = XboxController(s);
+            RightActionText.text = $"{text}";
+            print("done");
+            operation.Dispose();
+            playerInput.SwitchCurrentActionMap("Player1");
+        })
+        .Start();
+    }
+
+    public void RebindingDash(){//重新綁定Dash按鍵
+        print("Rebinding");
+        playerInput.SwitchCurrentActionMap("NotMe");
+        DashActionText.text = ("請輸入...");
+        Rebinding(DashActionRef,DashActionText);
+    }
+
+    public void RebindingLift(){//重新綁定Lift按鍵
+        print("Rebinding");
+        playerInput.SwitchCurrentActionMap("NotMe");
+        LiftActionText.text = ("請輸入...");
+        Rebinding(LiftActionRef,LiftActionText);
+    }
+    
+    public void RebindingThrow(){//重新綁定Throw按鍵
+        print("Rebinding");
+        playerInput.SwitchCurrentActionMap("NotMe");
+        ThrowActionText.text = ("請輸入...");
+        Rebinding(ThrowActionRef,ThrowActionText);
+    }
+
+    public void Rebinding(InputActionReference reference,Text actionText){//重新綁定
+        reference.action.PerformInteractiveRebinding()
+        .OnMatchWaitForAnother(0.1f)
+        .WithCancelingThrough("<keyboard>/escape")
+        .OnComplete(operation=>{
+            var p = operation.action.bindings[0].effectivePath;
+            var s = InputControlPath.ToHumanReadableString(p,InputControlPath.HumanReadableStringOptions.OmitDevice);
+            string text = XboxController(s);
+            actionText.text = $"{text}";
+            print("done");
+            operation.Dispose();
+            playerInput.SwitchCurrentActionMap("Player1");
+        })
+        .OnCancel(operation=>{
+            var p = operation.action.bindings[0].effectivePath;
+            var s = InputControlPath.ToHumanReadableString(p,InputControlPath.HumanReadableStringOptions.OmitDevice);
+            string text = XboxController(s);
+            actionText.text = $"{text}";
+            print("done");
+            operation.Dispose();
+            playerInput.SwitchCurrentActionMap("Player1");
+        })
+        .Start();
+    }
+
+    public string XboxController(string input){//修正Xbox手把按鈕名稱
+        string s = input;
+        if (s == "Button South")
+        {
+            s = "A";
+        }
+        if (s == "Button North")
+        {
+            s = "Y";
+        }
+        if (s == "Button East")
+        {
+            s = "B";
+        }
+        if (s == "Button West")
+        {
+            s = "X";
+        }
+
+        return s;
+    }
+
+    public void SaveRebinding(){//儲存綁定後按鍵配置
+        string AllActionBindingDate = playerInput.actions.SaveBindingOverridesAsJson();
+        PlayerPrefs.SetString("binding",AllActionBindingDate);
     }
 }
